13f7aaf921ea042897be9eb4feaffec1834d5357
Kurt LaBlanc <70603294+lablanck@users.noreply.github.com>
2020-09-30 20:02:13 -0400

    Add files via upload

---
+++ flix1/flix1/MoviesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesViewController.swift
+//  flix1
+//
+//  Created by Kurt LaBlanc on 9/30/20.
+//
+
+import Foundation
---
+++ flix1/flix1Tests/flix1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  flix1Tests.swift
+//  flix1Tests
+//
+//  Created by Kurt LaBlanc on 9/30/20.
+//
+
+import XCTest
+@testable import flix1
+
+class flix1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
